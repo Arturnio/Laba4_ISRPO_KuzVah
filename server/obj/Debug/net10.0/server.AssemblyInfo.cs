de3a907a31af953db6a620ef6a4b646626de53f7
Artur <only.up.ak@gmail.com>
2026-01-30 23:35:33 +0300

    add screneshots

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2d11c6c85a45457e7ec0ff3eddf14ec4624aa9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aab7f3ab32b8399105e7aed58b9f58db3333e306")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
